e1bcdfa54db5b55dce664cf8b0b804f3df651382
ahnaf-khan-82 <ahnafabrarkhan14@gmail.com>
2025-09-09 20:29:12 -0400

    Assignment 1 completed.

---
+++ Assignment  1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Assignment1;
+
+namespace Assignment1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+          using (Game game = new Game())
+          {
+            game.Run();
+          }
+        }
+    }
+}
