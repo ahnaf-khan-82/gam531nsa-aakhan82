de8c3045205c65fa73a0f865e73062bae0794f0f
ahnaf-khan-82 <ahnafabrarkhan14@gmail.com>
2025-10-07 22:05:35 -0400

    Assignment 3 completed.

--- Assignment 2/Program.cs
+++ Assignment 2/Program.cs
@@ -21,14 +21,13 @@ namespace WindowOpenTK
 
         private int _vao, _vbo, _program;
         private int _uModel, _uView, _uProj, _uColor;
-
         private float _t;
 
         private static readonly float[] Vert = {
-            -0.25f,  0.60f, 0f,  
-            -0.25f, -0.60f, 0f,   
-             0.25f,  0.60f, 0f,  
-             0.25f, -0.60f, 0f  
+            -0.25f,  0.60f, 0f,
+            -0.25f, -0.60f, 0f,
+             0.25f,  0.60f, 0f,
+             0.25f, -0.60f, 0f
         };
 
         public Game()
@@ -63,17 +62,14 @@ namespace WindowOpenTK
             _uView = GL.GetUniformLocation(_program, "uView");
             _uProj = GL.GetUniformLocation(_program, "uProj");
             _uColor = GL.GetUniformLocation(_program, "uColor");
-
             _vao = GL.GenVertexArray();
             _vbo = GL.GenBuffer();
 
             GL.BindVertexArray(_vao);
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
             GL.BufferData(BufferTarget.ArrayBuffer, Vert.Length * sizeof(float), Vert, BufferUsageHint.StaticDraw);
-
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
             GL.EnableVertexAttribArray(0);
-
             GL.BindVertexArray(0);
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
         }
@@ -94,31 +90,28 @@ namespace WindowOpenTK
         protected override void OnRenderFrame(FrameEventArgs args)
         {
             base.OnRenderFrame(args);
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             GL.UseProgram(_program);
             GL.BindVertexArray(_vao);
-
-
             GL.Uniform4(_uColor, new Vector4(0f, 0f, 1f, 1f));
 
 
             float aspect = Size.X / (float)Size.Y;
             Matrix4 view = Matrix4.LookAt(new Vector3(0, 0, 3), Vector3.Zero, Vector3.UnitY);
             Matrix4 proj = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(60f), aspect, 0.1f, 100f);
+
+
             GL.UniformMatrix4(_uView, false, ref view);
             GL.UniformMatrix4(_uProj, false, ref proj);
 
 
-            float angle = MathHelper.DegreesToRadians(90f) * _t;                
-            float scale = 1.0f + 0.20f * MathF.Sin(MathF.Tau * 1.0f * _t);    
+            float angle = MathHelper.DegreesToRadians(90f) * _t;
+            float scale = 1.0f + 0.20f * MathF.Sin(MathF.Tau * 1.0f * _t);
             Matrix4 model = Matrix4.CreateScale(scale) * Matrix4.CreateRotationY(angle);
 
             GL.UniformMatrix4(_uModel, false, ref model);
-
-
             GL.DrawArrays(PrimitiveType.TriangleStrip, 0, 4);
-
             GL.BindVertexArray(0);
             SwapBuffers();
         }
@@ -137,15 +130,18 @@ namespace WindowOpenTK
             GL.ShaderSource(vs, vsSrc);
             GL.CompileShader(vs);
             GL.GetShader(vs, ShaderParameter.CompileStatus, out var okV);
+            
             if (okV == 0) throw new Exception("Vertex shader: " + GL.GetShaderInfoLog(vs));
 
             int fs = GL.CreateShader(ShaderType.FragmentShader);
+
             GL.ShaderSource(fs, fsSrc);
             GL.CompileShader(fs);
             GL.GetShader(fs, ShaderParameter.CompileStatus, out var okF);
-            if (okF == 0) throw new Exception("Fragment shader: " + GL.GetShaderInfoLog(fs));
 
+            if (okF == 0) throw new Exception("Fragment shader: " + GL.GetShaderInfoLog(fs));
             int prog = GL.CreateProgram();
+
             GL.AttachShader(prog, vs);
             GL.AttachShader(prog, fs);
             GL.LinkProgram(prog);
@@ -154,6 +150,7 @@ namespace WindowOpenTK
             GL.DetachShader(prog, fs);
             GL.DeleteShader(vs);
             GL.DeleteShader(fs);
+            
             if (okP == 0) throw new Exception("Program link: " + GL.GetProgramInfoLog(prog));
             return prog;
         }
